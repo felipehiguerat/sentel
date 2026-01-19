3807dd610e1998fab4f040ebca204a08b76d0b71
Felipe Higuera <felipehiguera27@gmail.com>
2026-01-19 11:00:38 -0500

    cambio en seguridad de redis client 3

--- backend/app/db/redis_client.py
+++ backend/app/db/redis_client.py
@@ -1,10 +1,17 @@
 import redis.asyncio as redis
+import ssl  # <--- IMPORTANTE: Agrega esto
 from app.core.config import settings
 
-# Volvemos a la versión simple
+# 1. Creamos un contexto SSL que no verifique certificados ("Inseguro" pero funciona)
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+# 2. Pasamos ese contexto explícitamente
 redis_pool = redis.ConnectionPool.from_url(
     settings.REDIS_URL, 
-    decode_responses=True
+    decode_responses=True,
+    ssl_context=ssl_context  # <--- AQUÍ ESTÁ LA SOLUCIÓN
 )
 
 async def get_redis():
